a4064fda221ec4146b0f44a4f1fe546378bd2269
TheophileGreg <theophile.gregoire@icloud.com>
2020-09-09 17:40:18 +0200

    implement rollDice method

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.roll_button)
+
         rollButton.setOnClickListener{
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT ).show()
+            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT ).show()
+            rollDice()
 
         }
+    }
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+        val resultTextView = findViewById<TextView>(R.id.result_text)
+        resultTextView.text = randomInt.toString()
     }
 }
